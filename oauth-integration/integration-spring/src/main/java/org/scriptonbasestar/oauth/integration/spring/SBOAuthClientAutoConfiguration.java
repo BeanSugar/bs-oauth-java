c4a652314a7a38c4fa45defa2b5c2f8625f31c85
chaeeung.e <chaeeung.e@solarconnect.kr>
2017-09-12 12:57:25 +0900

    스프링 오엿클라이언트 뼈다기

---
+++ oauth-integration/integration-spring/src/main/java/org/scriptonbasestar/oauth/integration/spring/SBOAuthClientAutoConfiguration.java
@@ -0,0 +1,19 @@
+package org.scriptonbasestar.oauth.integration.spring;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author chaeeung.e
+ * @since 2017-09-12
+ */
+@Configuration
+public class SBOAuthClientAutoConfiguration {
+
+	//controller not filter
+	//post constructor security open.
+
+	//client bean by -- setting. connector name
+	//property 부족하면 에라
+	//
+
+}
