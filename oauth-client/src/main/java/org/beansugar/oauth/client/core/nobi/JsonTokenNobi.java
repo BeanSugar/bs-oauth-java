cf121efcfcbab75c32ed8c4f368c4add51400cc0
chaeeung.e <chaeeung.e@solarconnect.kr>
2017-09-13 11:11:28 +0900

    null 처ri ignore 추가

--- oauth-client/src/main/java/org/beansugar/oauth/client/core/nobi/JsonTokenNobi.java
+++ oauth-client/src/main/java/org/beansugar/oauth/client/core/nobi/JsonTokenNobi.java
@@ -20,7 +20,8 @@ public class JsonTokenNobi implements TokenNobi {
 				map.get("access_token").toString(),
 				map.get("expires_in")==null?0:Double.parseDouble(map.get("expires_in").toString()),
 				map.get("token_type")==null?null:AccessTokenType.valueOf(map.get("token_type").toString().toUpperCase()),
-				map.get("refresh_token").toString()
+				map.get("id_token")==null?null:map.get("id_token").toString(),
+				map.get("refresh_token")==null?null:map.get("refresh_token").toString()
 		);
 	}
 }
